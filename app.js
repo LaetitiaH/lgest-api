b59c76b4c1860f6de2195d175cf53b7977e9df2d
Laetitia Hars <laetitia.hars@gmail.com>
2019-09-04 13:11:45 +0200

    :sparkles: add typeslignescomptable API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const societesRouter = require('./resources/societes');
+const typesLignesComptablesRouter = require('./resources/types-lignes-comptables');
 
 module.exports = database => {
     const app = express();
@@ -11,6 +12,7 @@ module.exports = database => {
     app.use(express.urlencoded({ extended: true }));
 
     app.use('/societes', societesRouter(database));
+    app.use('/types-lignes-comptables', typesLignesComptablesRouter(database));
     //add others resources here
 
     return app;
